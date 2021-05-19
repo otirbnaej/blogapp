787377b0f4ba196e8e13754bd6eb1a957c8b0735
Jean Brito Araujo <otirbnaej@hotmail.com>
2021-05-19 20:07:10 -0300

    post submit and validate

--- routes/admin.js
+++ routes/admin.js
@@ -3,24 +3,28 @@ const router = express.Router();
 const mongoose = require('mongoose');
 require('../models/Categoria');
 const Categoria = mongoose.model('categorias');
+require('../models/Postagem');
+const Postagem = mongoose.model('postagens');
 
 // router.get('/', (req, res) => {
-//     res.render("admin/index");
+//     res.render('admin/index');
 // });
 
 // router.get('/posts', (req, res) => {
-//     res.send("Página de posts");
+//     res.send('Página de posts');
 // });
 
 router.get('/categorias', (req, res) => {
-    Categoria.find().sort({date: "desc"}).lean()
+    Categoria.find().sort({
+            date: 'desc'
+        }).lean()
         .then((categorias) => {
-            res.render("admin/categorias", {
+            res.render('admin/categorias', {
                 categorias
             });
         }).catch(e => {
-            req.flash("error_msg", "Houve um erro ao listar as categorias.")
-            res.redirect("/admin");
+            req.flash('error_msg', 'Houve um erro ao listar as categorias.')
+            res.redirect('/admin');
         });
 });
 
@@ -33,18 +37,20 @@ router.post('/categorias/nova', (req, res) => {
     var erros = [];
     if (!req.body.nome) {
         erros.push({
-            texto: "Nome inválido"
+            texto: 'Nome inválido'
         });
     }
 
     if (!req.body.slug) {
         erros.push({
-            texto: "Slug inválido"
+            texto: 'Slug inválido'
         });
     }
 
     if (erros.length > 0)
-        res.render("admin/addcategorias", { erros });
+        res.render('admin/addcategorias', {
+            erros
+        });
     else {
         if (req.body.id) { // Se houver um id, o item deve ser editado.
             Categoria.findOne({
@@ -55,15 +61,15 @@ router.post('/categorias/nova', (req, res) => {
                     categoria.slug = req.body.slug;
 
                     categoria.save().then(() => {
-                        req.flash("success_msg", "Categoria editada com sucesso");
-                        res.redirect("/admin/categorias");
+                        req.flash('success_msg', 'Categoria editada com sucesso');
+                        res.redirect('/admin/categorias');
                     }).catch(err => {
-                        req.flash("error_msg", "Houve um erro interno ao salvar a edição da categoria.");
-                        res.redirect("/admin/categorias");
+                        req.flash('error_msg', 'Houve um erro interno ao salvar a edição da categoria.');
+                        res.redirect('/admin/categorias');
                     })
                 }).catch((err) => {
-                    req.flash("error_msg", "Houve um erro ao editar a categoria");
-                    res.redirect("/admin/categorias");
+                    req.flash('error_msg', 'Houve um erro ao editar a categoria');
+                    res.redirect('/admin/categorias');
                 })
 
         } else { // Caso não haja id, um novo item é criado.
@@ -74,25 +80,27 @@ router.post('/categorias/nova', (req, res) => {
 
             new Categoria(novaCategoria).save()
                 .then(() => {
-                    req.flash("success_msg", "Categoria criada com sucesso!");
-                    res.redirect("/admin/categorias");
+                    req.flash('success_msg', 'Categoria criada com sucesso!');
+                    res.redirect('/admin/categorias');
                 })
                 .catch(() => {
-                    req.flash("error_msg", "Houve um erro ao salvar a categoria. Tente novamente.");
-                    res.redirect("/admin");
+                    req.flash('error_msg', 'Houve um erro ao salvar a categoria. Tente novamente.');
+                    res.redirect('/admin');
                 });
         }
     };
 });
 
-router.post("/categorias/deletar", (req, res) => {
-    Categoria.findOneAndDelete({_id: req.body.id})
+router.post('/categorias/deletar', (req, res) => {
+    Categoria.findOneAndDelete({
+            _id: req.body.id
+        })
         .then(() => {
-            req.flash("success_msg", "Categoria deletada com sucesso!");
-            res.redirect("/admin/categorias");
+            req.flash('success_msg', 'Categoria deletada com sucesso!');
+            res.redirect('/admin/categorias');
         }).catch((err) => {
-            req.flash("error_msg", "Houve um erro ao deletar a categoria");
-            res.redirect("/admin/categorias");
+            req.flash('error_msg', 'Houve um erro ao deletar a categoria');
+            res.redirect('/admin/categorias');
         });
 })
 
@@ -107,9 +115,9 @@ router.get('/categorias/edit/:id', (req, res) => {
             res.render('admin/addcategorias', {
                 categoria
             });
-        }).catch((e) => {
-            req.flash("error_msg", "Esta categoria não existe.");
-            res.redirect("/admin/categorias");
+        }).catch(() => {
+            req.flash('error_msg', 'Esta categoria não existe.');
+            res.redirect('/admin/categorias');
         });
 
 });
@@ -117,12 +125,95 @@ router.get('/categorias/edit/:id', (req, res) => {
 // postagens
 
 router.get('/postagens', (req, res) => {
-    res.render("admin/postagens");
+    res.render('admin/postagens');
 })
 
 router.get('/postagens/add', (req, res) => {
-    res.render('admin/addpostagem');
-})
+    Categoria.find().lean()
+        .then((categorias) => {
+            res.render('admin/addpostagem', {
+                categorias
+            });
+        }).catch((e) => {
+            req.flash('error_msg', 'Houve um erro ao carregar o formulário');
+            req.redirect('/admin');
+        });
+});
+
+router.post('/postagens/nova', (req, res) => {
+
+    var erros = [];
+    if (!req.body.titulo) {
+        erros.push({
+            texto: 'Informe um título.'
+        });
+    }
+
+    if (!req.body.slug) {
+        erros.push({
+            texto: 'Slug inválido.'
+        });
+    }
+
+    if (!req.body.descricao) {
+        erros.push({
+            texto: 'Descrição inválida.'
+        });
+    }
+
+    if (erros.length > 0) {
+        Categoria.find().lean()
+        .then((categorias) => {
+            res.render('admin/addpostagem', {
+                categorias, erros
+            });
+        }).catch(() => {
+            req.flash('error_msg', 'Houve um erro ao carregar o formulário');
+            req.redirect('/admin');
+        });
+    }
+    else {
+        if (req.body.id) { // Se houver um id, o item deve ser editado.
+            Postagem.findOne({
+                    _id: req.body.id
+                })
+                .then((postagem) => {
+                    postagem.nome = req.body.nome;
+                    postagem.slug = req.body.slug;
+
+                    postagem.save().then(() => {
+                        req.flash('success_msg', 'Postagem editada com sucesso');
+                        res.redirect('/admin/postagens');
+                    }).catch(() => {
+                        req.flash('error_msg', 'Houve um erro interno ao salvar a edição da postagem.');
+                        res.redirect('/admin/postagens');
+                    })
+                }).catch(() => {
+                    req.flash('error_msg', 'Houve um erro ao editar a postagem');
+                    res.redirect('/admin/postagens');
+                })
+
+        } else { // Caso não haja id, um novo item é criado.
+            const novaPostagem = {
+                titulo: req.body.titulo,
+                slug: req.body.slug,
+                descricao: req.body.descricao,
+                conteudo: req.body.conteudo,
+                categoria: req.body.categoria
+            }
+
+            new Postagem(novaPostagem).save()
+                .then(() => {
+                    req.flash('success_msg', 'Postagem criada com sucesso!');
+                    res.redirect('/admin/postagens');
+                })
+                .catch(() => {
+                    req.flash('error_msg', 'Houve um erro ao salvar a postagem. Tente novamente.');
+                    res.redirect('/admin');
+                });
+        }
+    };
+});
 
 
 module.exports = router;
