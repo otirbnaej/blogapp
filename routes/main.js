de7058f2836d1a90bbffca034666cc8addf5e4d6
Jean Brito Araujo <otirbnaej@hotmail.com>
2021-05-22 08:46:26 -0300

    added content to main page

--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+require('../models/Categoria');
+const Categoria = mongoose.model('categorias');
 require('../models/Postagem');
 const Postagem = mongoose.model('postagens');
 
 router.get('/', (req, res) => {
-    Postagem.find().populate('categoria').sort({data: 'desc'}).lean()
+    Postagem.find().populate('categoria').sort({
+        date: 'desc'
+    }).lean()
         .then((postagens) => {
-            res.render('./main/home', postagens);
-        }).catch((e) => {
+            res.render('./main/home', { postagens });
+        }).catch(() => {
             req.flash('error_msg', "Houve um erro interno.");
-            res.render('./erros/404');
+            res.redirect('/404');
         });
+});
+
+router.get('/404', (req, res) => {
+    res.render('./erros/404');
 });
 
 module.exports = router;
